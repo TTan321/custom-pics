92e051ffcfacf869eb6c0c6487e43cb2deb120b4
Tony Tan <tony@Tonys-MacBook-Air.local>
2024-09-26 16:32:48 -0700

    fixed typo max to max_length in profile class, added custompics app to setting.py

--- custompics/models.py
+++ custompics/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
 class Profile(models.Model):
-    firstname = models.CharField(max=256)
-    lastname = models.CharField(max=256)
-    email - models.EmailField()
+    firstname = models.CharField(max_length=256)
+    lastname = models.CharField(max_length=256)
+    email = models.EmailField()
     createdat = models.DateTimeField(auto_now_add=True)
-    
